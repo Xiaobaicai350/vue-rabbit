032c4d256cd952cc6550687a9d807c3ad8b0989d
胥天昊 <1499487526@qq.com>
2023-06-19 09:46:06 +0800

    引入axios并简单封装

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import { getCategoryAPI } from "@/apis/testAPI.js";
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+getCategoryAPI().then((res) => {
+  console.log(res);
+});
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/utils/http.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+// 创建axios实例
+const httpInstance = axios.create({
+  baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+  timeout: 5000,
+});
+
+//目前这两个没啥用，以后再用
+// axios请求拦截器
+httpInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
+
+// axios响应式拦截器
+httpInstance.interceptors.response.use(
+  (res) => res.data,
+  (e) => {
+    return Promise.reject(e);
+  }
+);
+
+export default httpInstance;
