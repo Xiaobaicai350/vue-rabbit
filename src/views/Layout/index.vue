dbaa07dd0213734f8bbbbfd2db1112f949297b24
胥天昊 <1499487526@qq.com>
2023-06-19 10:56:35 +0800

    scss变量自动导入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>我是layout</div>
+  <div class="test">我是layout</div>
   <!-- 添加二级路由出口 -->
   <router-view />
 </template>
@@ -8,4 +8,8 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+</style>
