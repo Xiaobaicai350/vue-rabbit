cde258114e09eeb183e65f67fa9ca267bf53e167
胥天昊 <1499487526@qq.com>
2023-06-19 10:44:47 +0800

    增加一些静态资源

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-
+import "@/styles/common.scss";
 import { getCategoryAPI } from "@/apis/testAPI.js";
 const app = createApp(App);
 
