4b651ffe296ae2ef3e4cd4b4d5326d15dd8c65e9
胥天昊 <1499487526@qq.com>
2023-06-24 20:21:49 +0800

    完成分类实现

---
+++ src/views/Home/components/HomeNew.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>HomeNew</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,17 @@
-<template>
-  <div style="height: 1200px">我是home</div>
-</template>
-
-<script>
-export default {};
+<script setup>
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import homeProduct from "./components/HomeProduct.vue";
 </script>
 
-<style></style>
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+</template>
