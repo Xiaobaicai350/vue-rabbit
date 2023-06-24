688c14f0c90c68ec203c87f7e9f3c087ec388b27
胥天昊 <1499487526@qq.com>
2023-06-24 22:22:17 +0800

    完成面包屑

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,9 +1,105 @@
-<template>
-  <div>我是categories</div>
-</template>
-
-<script>
-export default {};
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { getTopCategoryAPI } from "@/apis/category";
+const categoryData = ref({});
+// 这里可以获取路由的参数，这里还没引入，注意一下
+const route = useRoute();
+const getCategory = async () => {
+  // 如何在setup中获取路由参数 useRoute() -> route 等价于this.$route
+  const res = await getTopCategoryAPI(route.params.id);
+  categoryData.value = res.result;
+};
+onMounted(() => {
+  console.log("这里是category");
+  getCategory();
+});
 </script>
 
-<style></style>
+<template>
+  <div class="bread-container">
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
+    }
+  }
+
+  .bread-container {
+    padding: 25px 0;
+  }
+}
+</style>
