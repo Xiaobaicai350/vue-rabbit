3515fe263ca2f2ac90a02f57080929cca8272b76
胥天昊 <1499487526@qq.com>
2023-06-24 22:30:53 +0800

    更新category的banner

--- src/apis/home.js
+++ src/apis/home.js
@@ -4,9 +4,14 @@
  * @return {*}
  */
 import httpInstance from "@/utils/http";
-export function getBannerAPI() {
+export function getBannerAPI(params = {}) {
+  // 默认为1 商品为2
+  const { distributionSite = "1" } = params;
   return httpInstance({
-    url: "home/banner",
+    url: "/home/banner",
+    params: {
+      distributionSite,
+    },
   });
 }
 /**
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import { getBannerAPI } from "@/apis/home";
 import { useRoute } from "vue-router";
 import { getTopCategoryAPI } from "@/apis/category";
 const categoryData = ref({});
@@ -10,22 +11,57 @@ const getCategory = async () => {
   const res = await getTopCategoryAPI(route.params.id);
   categoryData.value = res.result;
 };
+// 获取banner
+const bannerList = ref([]);
+
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: "2",
+  });
+  console.log(res);
+  bannerList.value = res.result;
+};
+
 onMounted(() => {
+  getBanner();
   console.log("这里是category");
   getCategory();
 });
 </script>
 
 <template>
-  <div class="bread-container">
-    <el-breadcrumb separator=">">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
-    </el-breadcrumb>
+  <div class="top-category">
+    <div class="container m-top-20">
+      <!-- 面包屑 -->
+      <div class="bread-container">
+        <el-breadcrumb separator=">">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+// 部分代码省略
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 .top-category {
   h3 {
     font-size: 28px;
