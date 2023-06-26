8a97a71bcea22b92431f0718570ac6035e25f331
胥天昊 <1499487526@qq.com>
2023-06-26 17:10:03 +0800

    无线加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -38,6 +38,20 @@ const tabChange = () => {
   reqData.value.page = 1;
   getGoodList();
 };
+
+// 加载更多
+const disabled = ref(false);
+const load = async () => {
+  console.log("加载更多数据咯");
+  // 获取下一页的数据
+  reqData.value.page++;
+  const res = await getSubCategoryAPI(reqData.value);
+  goodList.value = [...goodList.value, ...res.result.items];
+  // 加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true;
+  }
+};
 </script>
 
 <template>
@@ -58,7 +72,11 @@ const tabChange = () => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div
+        class="body"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+      >
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
