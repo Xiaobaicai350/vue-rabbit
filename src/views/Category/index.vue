d01ce66f18ce683e00efbeafa00bbd8c1a4da1c6
胥天昊 <1499487526@qq.com>
2023-06-26 12:49:40 +0800

    添加分类数据模板

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from "vue";
 import { getBannerAPI } from "@/apis/home";
 import { useRoute } from "vue-router";
 import { getTopCategoryAPI } from "@/apis/category";
+import GoodsItem from "../Home/components/GoodsItem.vue";
 const categoryData = ref({});
 // 这里可以获取路由的参数，这里还没引入，注意一下
 const route = useRoute();
@@ -46,6 +47,30 @@ onMounted(() => {
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!--  -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="ref-goods"
+        v-for="item in categoryData.children"
+        :key="item.id"
+      >
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
