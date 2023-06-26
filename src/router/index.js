36d2ec65cd8bccaebbb24c1daf7140055683612a
胥天昊 <1499487526@qq.com>
2023-06-26 14:18:43 +0800

    二级分类

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Login from "@/views/Login/index.vue";
 import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
+import SubCategory from "@/views/SubCategory/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
           path: "category/:id",
           name: "category",
           component: Category,
+        },
+        {
+          path: "category/sub/:id",
+          name: "subCategory",
+          component: SubCategory,
         },
       ],
     },
