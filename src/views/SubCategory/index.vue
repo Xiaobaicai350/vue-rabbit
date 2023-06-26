189337e4672a488eecff2381bebdb87e01f6c12a
胥天昊 <1499487526@qq.com>
2023-06-26 16:49:35 +0800

    完成列表筛选实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -32,6 +32,12 @@ const getGoodList = async () => {
 };
 
 onMounted(() => getGoodList());
+// tab切换回调
+const tabChange = () => {
+  console.log("tab切换了", reqData.value.sortField);
+  reqData.value.page = 1;
+  getGoodList();
+};
 </script>
 
 <template>
@@ -47,7 +53,7 @@ onMounted(() => getGoodList());
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
