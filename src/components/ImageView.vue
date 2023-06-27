c38f927da4cea6131214de4d05736289fa998cf8
胥天昊 <1499487526@qq.com>
2023-06-27 14:03:36 +0800

    完成图片预览组件

--- src/components/ImageView.vue
+++ src/components/ImageView.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref, watch } from "vue";
+import { useMouseInElement } from "@vueuse/core";
+
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -7,28 +10,79 @@ const imageList = [
   "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg",
 ];
-import { ref } from "vue";
 
-// 实现鼠标移入交互
-const curIndex = ref(0);
-const mouseEnterFn = (i) => (curIndex.value = i);
+// 1.小图切换大图显示
+const activeIndex = ref(0);
+
+const enterhandler = (i) => {
+  activeIndex.value = i;
+};
+
+// 2. 获取鼠标相对位置
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+
+// 3. 控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化 重新设置left/top）
+const left = ref(0);
+const top = ref(0);
+
+const positionX = ref(0);
+const positionY = ref(0);
+watch([elementX, elementY, isOutside], () => {
+  console.log("xy变化了");
+  // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+  if (isOutside.value) return;
+  console.log("后续逻辑执行了");
+  // 有效范围内控制滑块距离
+  // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100;
+  }
+  // 纵向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100;
+  }
+
+  // 处理边界
+  if (elementX.value > 300) {
+    left.value = 200;
+  }
+  if (elementX.value < 100) {
+    left.value = 0;
+  }
+
+  if (elementY.value > 300) {
+    top.value = 200;
+  }
+  if (elementY.value < 100) {
+    top.value = 0;
+  }
+
+  // 控制大图的显示
+  positionX.value = -left.value * 2;
+  positionY.value = -top.value * 2;
+});
 </script>
 
 <template>
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
-      <img :src="imageList[0]" alt="" />
+      <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div
+        class="layer"
+        v-show="!isOutside"
+        :style="{ left: `${left}px`, top: `${top}px` }"
+      ></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
       <li
         v-for="(img, i) in imageList"
         :key="i"
-        @mouseenter="mouseEnterFn(i)"
-        :class="{ active: i === curIndex }"
+        @mouseenter="enterhandler(i)"
+        :class="{ active: i === activeIndex }"
       >
         <img :src="img" alt="" />
       </li>
@@ -39,11 +93,11 @@ const mouseEnterFn = (i) => (curIndex.value = i);
       :style="[
         {
           backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`,
+          backgroundPositionX: `${positionX}px`,
+          backgroundPositionY: `${positionY}px`,
         },
       ]"
-      v-show="false"
+      v-show="!isOutside"
     ></div>
   </div>
 </template>
