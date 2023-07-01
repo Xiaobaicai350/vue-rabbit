44f35b3c00157c619886f711739cf2bbda09c180
胥天昊 <1499487526@qq.com>
2023-07-01 19:56:53 +0800

    m

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "@/styles/common.scss";
+// 导入pinia的持久化插件
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 const app = createApp(App);
 // 引入全局组件插件
 import { componentPlugin } from "@/components";
-
+const pinia = createPinia();
+// 用pinia的持久化插件
+pinia.use(piniaPluginPersistedstate);
 app.use(componentPlugin);
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 // 全局指令注册
 import { lazyPlugin } from "@/directives";
