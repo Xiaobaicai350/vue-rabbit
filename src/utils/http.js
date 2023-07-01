00f2c9ff5f3b3bdcdb25fc48614af171b81ec135
胥天昊 <1499487526@qq.com>
2023-07-01 20:56:34 +0800

    m

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
-
+import { useUserStore } from "@/stores/user.js";
 // 创建axios实例
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -12,6 +12,21 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
+
+// axios请求拦截器
+httpInstance.interceptors.request.use(
+  (config) => {
+    // 1. 从pinia获取token数据
+    const userStore = useUserStore();
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e)
